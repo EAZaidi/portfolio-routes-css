630f9636ded09e40254cb7b4d4fae6f36a2bf932
Essa Zaidi <160016180+EAZaidi@users.noreply.github.com>
2024-11-11 00:56:03 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import styles from "../app/page.module.css"; // Import the module CSS
+
 
 
 const geistSans = localFont({
