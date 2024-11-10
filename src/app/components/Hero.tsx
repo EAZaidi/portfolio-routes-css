747c0b0030764e1d957d42d3f01a5747701c0682
Essa Zaidi <160016180+EAZaidi@users.noreply.github.com>
2024-11-11 00:54:57 +0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className={styles.heroHidden}></div>
       <div className={styles.heroText}>
         <div>
-          <p data-aos="zoom-in">Hello, I'm</p>
+          <p data-aos="zoom-in">Hello, I&apos;m</p>
           <p data-aos="zoom-in">Essa</p>
           <p data-aos="zoom-in">Abbas Zaidi</p>
         </div>
